1797234d6bf9a6ff040d3877e9e2522c3a2fdd60
Josh Mejia <joshua.mejia@gmail.com>
2013-05-24 22:48:37 -0600

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sparklr::Application.config.secret_token = 'ec5aa8f62ceebb5948269f1fabc2a6b38e28c7c43e8c026c9f33a2768a4bd7906a8ba2633d890280eab47278b493ea9cae23234188b26a997853191fca671990'
